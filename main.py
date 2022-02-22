777ccca02e48f4777560adda8a7bdbeca635c2a6
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-02-22 10:14:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from info import SESSION, APP_ID, API_HASH, BOT_TOKEN
 plugins = dict(root="plugins")
 Bot = Client(
     session_name=SESSION,
-    api_id=API_ID,
+    api_id=APP_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
     plugins=plugins,
