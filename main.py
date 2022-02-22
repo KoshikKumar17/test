cc28cb7447881cc2280c4ca9cfc2bf330d5ad20f
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-02-22 06:25:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# (c) @TheTgRoBots
+
+import os
+from pyrogram import Client
+from info import SESSION, APP_ID, API_HASH, BOT_TOKEN
+
+Bot = Client(
+    session_name=SESSION,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins = dict(root="plugins")
+)
+
+
+Bot.run()
