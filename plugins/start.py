b209b94055500aa9373719bc2b7a0169ea186aef
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-02-22 12:13:42 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,36 +1,15 @@
 # (c) @TheTgRoBots
 import os
 from Script import script
+from Buttons import buttons
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
-
-START_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('🤔 Help 🤔', callback_data='help'),
-        InlineKeyboardButton('😇 About 😇', callback_data='about')
-        ],[
-        InlineKeyboardButton('↗️ Channel ↗️', url='https://t.me/thetgrobots'),
-        InlineKeyboardButton('⭕ GitHub', url='https://github.com/The-TG-RoBots')
-        ],[
-        InlineKeyboardButton('❌ Close ❌', callback_data='close')
-        ]]
-    )
-HELP_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('⬅️ Back', callback_data='start')
-        ]]
-    )
-ABOUT_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('⬅️ Back', callback_data='start')
-        ]]
-    )
 
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
         thetgrobots = await update.reply_text("**Processing...⏸️**")
         await thetgrobots.edit_text(
         text=script.START_TEXT,
-        reply_markup = START_BUTTONS,
-       disable_web_page_preview = True
+        reply_markup=buttons.START_BUTTONS,
+       disable_web_page_preview=True
     )
