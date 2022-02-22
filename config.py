2177271ae85faa2597befe1ccb38d8ac113b492c
• Kŕrish <83119148+HKrrish@users.noreply.github.com>
2022-02-22 22:11:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ class Config(object):
   APP_ID = int(os.environ.get("APP_ID", ""))
   API_HASH = os.environ.get("API_HASH", "")
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+  LOGGER = true
+  JOIN_LOGGER = (-1001232661249)
+  OWNER_ID = 1857338892
+  
