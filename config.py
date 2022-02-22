6ab60c155f8b454918965de376ec844c0dcdca84
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-02-22 10:49:28 +0530

    Update and rename info.py to config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# (c) @TheTgRoBots
+# selling codes for money is strictly prohibited.
+
+# Bot information
+class Config(object):
+  APP_ID = int(os.environ.get("APP_ID", ""))
+  API_HASH = os.environ.get("API_HASH", "")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
--- info.py
+++
@@ -1,8 +0,0 @@
-# (c) @TheTgRoBots
-# selling codes for money is strictly prohibited.
-
-# Bot information
-SESSION = environ.get('SESSION', 'Group-Management-Bot')
-APP_ID = int(environ['APP_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
