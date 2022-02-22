752a1764aef297e70d5e4c86f26ca5836306f429
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-02-22 14:00:10 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,6 +4,7 @@ from Script import script
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 
+#buttons
 START_BUTTONS = InlineKeyboardMarkup(
         [[
         InlineKeyboardButton('🤔 Help 🤔', callback_data='help'),
@@ -25,6 +26,7 @@ ABOUT_BUTTONS = InlineKeyboardMarkup(
         InlineKeyboardButton('⬅️ Back', callback_data='start')
         ]]
     )
+#commands
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
         thetgrobots = await update.reply_text("**Processing...⏸️**")
@@ -33,3 +35,26 @@ async def start(bot, update):
         reply_markup=START_BUTTONS,
        disable_web_page_preview=True
     )
+#callback
+@Client.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "start":
+        await update.message.edit_text(
+            text=script.START_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=START_BUTTONS
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=script.HELP_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=HELP_BUTTONS
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=script.ABOUT_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=ABOUT_BUTTONS
+        )
+    elif update.data == "close":
+        await update.message.delete()
