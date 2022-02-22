46f3f693faffe65d45cb7ca3765a0b9da040a9be
• Kŕrish <83119148+HKrrish@users.noreply.github.com>
2022-02-22 20:14:28 +0530

    Delete wlcm_sql.py

--- plugins/Modules/Sql/SQL/wlcm_sql.py
+++
@@ -1,5 +0,0 @@
-import random, threading 
-from typing import Union 
-from sqlalchemy import BigInteger, Boolean, Column, Integer, String, UnicodeText
-from test.Modules.helper_funcs.message_types import Types
-from test.Modules.SQL import BASE, SESSION
