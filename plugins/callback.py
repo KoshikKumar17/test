3d87c8ca23c69206516f125e56b6e0465c6718b2
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-02-22 12:03:49 +0530

    Create callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -1,22 +1,28 @@
 # (c) @TheTgRoBots
 
+import os
+from Script import script
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+
+
 @Client.on_callback_query()
 async def cb_data(bot, update):
     if update.data == "start":
         await update.message.edit_text(
-            text=START_TEXT,
+            text=script.START_TEXT,
             disable_web_page_preview=True,
             reply_markup=START_BUTTONS
         )
     elif update.data == "help":
         await update.message.edit_text(
-            text=HELP_TEXT,
+            text=script.HELP_TEXT,
             disable_web_page_preview=True,
             reply_markup=HELP_BUTTONS
         )
     elif update.data == "about":
         await update.message.edit_text(
-            text=ABOUT_TEXT.format((await bot.get_me()).username),
+            text=script.ABOUT_TEXT,
             disable_web_page_preview=True,
             reply_markup=ABOUT_BUTTONS
         )
