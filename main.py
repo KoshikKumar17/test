589e5aaf844068c39d73730db376d0479018f524
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-02-22 10:14:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from pyrogram import Client
 from info import SESSION, APP_ID, API_HASH, BOT_TOKEN
 
+plugins = dict(root="plugins")
 Bot = Client(
     session_name=SESSION,
     api_id=API_ID,
