5a8164ab202f0bedf0a5019b0f302e5c4397e8bc
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-02-22 12:02:00 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -1,1 +1,24 @@
 # (c) @TheTgRoBots
+
+@Client.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "start":
+        await update.message.edit_text(
+            text=START_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=START_BUTTONS
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=HELP_BUTTONS
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_TEXT.format((await bot.get_me()).username),
+            disable_web_page_preview=True,
+            reply_markup=ABOUT_BUTTONS
+        )
+    elif update.data == "close":
+        await update.message.delete()
