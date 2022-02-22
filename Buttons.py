3fcdb834a75f9015fa9e04c0ebe94fb9381fa098
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-02-22 12:05:54 +0530

    Update Buttons.py

--- Buttons.py
+++ Buttons.py
@@ -2,3 +2,24 @@
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🤔 Help 🤔', callback_data='help'),
+        InlineKeyboardButton('😇 About 😇', callback_data='about')
+        ],[
+        InlineKeyboardButton('↗️ Channel ↗️', url='https://t.me/thetgrobots'),
+        InlineKeyboardButton('⭕ GitHub', url='https://github.com/The-TG-RoBots')
+        ],[
+        InlineKeyboardButton('❌ Close ❌', callback_data='close')
+        ]]
+    )
+HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('⬅️ Back', callback_data='start')
+        ]]
+    )
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('⬅️ Back', callback_data='start')
+        ]]
+    )
