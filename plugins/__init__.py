57267ff4745ece4d08f37d191fc0cc3aaf2df981
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-02-24 06:01:34 +0530

    Added Broadcast Database

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,6 +1,9 @@
-# (c) Sirius 
+# (c) Sirius and Koshik
 
+import os
+import threading
 from sqlalchemy import create_engine
+from sqlalchemy import Column, TEXT, Numeric
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -15,3 +18,37 @@ def start() -> scoped_session:
 
 BASE = declarative_base()
 SESSION = start()
+
+INSERTION_LOCK = threading.RLock()
+
+class Broadcast(BASE):
+    __tablename__ = "broadcast"
+    id = Column(Numeric, primary_key=True)
+    user_name = Column(TEXT)
+
+    def __init__(self, id, user_name):
+        self.id = id
+        self.user_name = user_name
+
+Broadcast.__table__.create(checkfirst=True)
+
+#  Add user details -
+async def add_user(id, user_name):
+    with INSERTION_LOCK:
+        msg = SESSION.query(Broadcast).get(id)
+        if not msg:
+            usr = Broadcast(id, user_name)
+            SESSION.add(usr)
+            SESSION.commit()
+        else:
+            pass
+
+async def full_userbase():
+    users = SESSION.query(Broadcast).all()
+    SESSION.close()
+    return users
+
+async def query_msg():
+    try:
+        query = SESSION.query(Broadcast.id).order_by(Broadcast.id)
+        return query
